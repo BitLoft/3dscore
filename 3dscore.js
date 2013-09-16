3122d3435d6eaee92c735605a1ba8171e096806c
Tj Houston <tj@tjhouston.com>
2013-09-16 19:59:35 -0400

    setup client folder

--- 3dscore.js
+++ 3dscore.js
@@ -1,5 +1,13 @@
 Template.scoreButtons.events(
   {       
     'click #5points':function(e){
-      alert('testing');
-}})
+      alert('5 Points');
+      },
+      'click #8points':function(e){
+      alert('8 Points');
+      },
+      'click #10points':function(e){
+      alert('10 Points');
+      },
+
+})
